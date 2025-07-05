0c3937126dffa2b533f94fdd0054d54361a1d4aa
Anastasiia Martynenko <martynenkoad4@gmail.com>
2025-07-06 01:01:20 +0300

    Written3 unit tests for shared logic

--- Incidenten.API/obj/Debug/net9.0/Incidenten.API.AssemblyInfo.cs
+++ Incidenten.API/obj/Debug/net9.0/Incidenten.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Incidenten.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7507deb123ab01c62c609e3167b8aef56204f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714002a680436cb80775195cdbc41de7f6fad37d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Incidenten.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Incidenten.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ IncidentenApp.Tests/DomainTests.cs
@@ -0,0 +1,13 @@
+﻿using Incidenten.Domain;
+
+namespace IncidentenApp.Tests;
+
+public class DomainTests
+{
+    [Fact]
+    public void IncidentImageEntity_DefaultImageUrlProvided_WhenIncidentImageClassIsInstantiated()
+    {
+        var incidentImage = new IncidentImage();
+        Assert.Equal("http://localhost:5000/images/NO_IMAGE_PLACEHOLDER.png", incidentImage.ImageUrl);
+    }
+}
